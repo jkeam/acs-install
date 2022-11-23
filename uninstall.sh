c0fc73e73d8b0b17f90d9bab644b6a69e6d82b4b
Jon Keam <jpkeam@gmail.com>
2022-11-23 13:44:43 -0500

    add back in missing sleep

--- uninstall.sh
+++ uninstall.sh
@@ -3,20 +3,18 @@
 oc delete -f ./6_create_secured_cluster.yaml
 date
 echo 'Deleting secured cluster, waiting for 10 minutes...'
-# sleep 600
+sleep 600
 echo ''
 
 oc delete -f ./5_create_central.yaml
 date
 echo 'Deleting central, waiting for 10 minutes...'
-# sleep 600
+sleep 600
 echo ''
 
 oc delete -f ./4_create_subscription.yaml
 oc delete -f ./3_create_operator_group.yaml
 oc delete -f ./2_create_stackrox_namespace.yaml
 oc delete -f ./1_create_operator_namespace.yaml
-
-sleep 5
 
 echo 'Done'
